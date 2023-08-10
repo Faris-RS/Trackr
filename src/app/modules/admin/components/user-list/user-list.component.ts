03ddb252891fedcb0bd0b81c39ee6df109d87c4b
Mohamed Faris <brotofarisrs@gmail.com>
2023-08-10 20:26:06 +0530

    finiallised data table

--- src/app/modules/admin/components/user-list/user-list.component.ts
+++ src/app/modules/admin/components/user-list/user-list.component.ts
@@ -15,7 +15,58 @@ export interface UserData {
 export class UserListComponent {
   users: UserData[] = [
     { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com' }
+    { id: 2, name: 'Johny Doe', email: 'john@example.com' },
+    { id: 3, name: 'Johner Doe', email: 'john@example.com' },
+    { id: 4, name: 'Jane Smith', email: 'jane@example.com' },
     // Add more user data
   ];
+
+  sortColumn: keyof UserData | null = null;
+  sortDirection: 'asc' | 'desc' = 'asc';
+
+  toggleSort(column: keyof UserData): void {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    this.applySortAndFilter(); // Apply both sorting and filtering
+  }
+
+  applySortAndFilter(): void {
+    this.applySort();
+    this.applyFilter();
+  }
+
+  applySort(): void {
+    if (this.sortColumn) {
+      this.users.sort((a, b) => {
+        const aValue = a[this.sortColumn as keyof UserData];
+        const bValue = b[this.sortColumn as keyof UserData];
+
+        if (typeof aValue === 'number' && typeof bValue === 'number') {
+          return this.sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
+        }
+
+        if (typeof aValue === 'string' && typeof bValue === 'string') {
+          return this.sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+        }
+
+        return 0;
+      });
+    }
+  }
+
+  searchText: string = '';
+  filteredUsers: UserData[] = this.users;
+
+  applyFilter(): void {
+    const filterValue = this.searchText.toLowerCase();
+    this.filteredUsers = this.users.filter(user =>
+      user.name.toLowerCase().includes(filterValue) ||
+      user.email.toLowerCase().includes(filterValue)
+    );
+  }
 }
